7a2fab0de8b6164de332984cbffb35f197fba29e
matheus110506 <matheus110506@hotmail.com>
2026-03-08 20:13:55 -0300

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 
 router.post('/login', adminController.loginAdmin);
-
 router.get('/logs', adminController.listarLogs);
-
 router.post('/banir-ip', adminController.banirIP);
+router.post('/banir-mae', adminController.banirMae);
+router.post('/banir-filho', adminController.banirFilho);
 
 module.exports = router;
